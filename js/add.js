7d329eadced187a751a4367d14c6b6fe10584238
asqarmadrimov <madrimov2530@gmail.com>
2022-07-28 13:07:17 +0500

    apple

---
+++ js/add.js
@@ -0,0 +1,15 @@
+vorsitetitle = alert ("Wlecome to Apple");
+
+console.log(prompt("Who is the CEO of Apple?"));
+console.log(prompt("Who are the founders of Apple?"));
+console.log(prompt("Where is Apple located?"));
+console.log(prompt("When was Steve Jobs born?"));
+console.log(prompt("When did Steve Jobs and his team create a personal computer?"));
+console.log(prompt("When was the iPad created?"));
+console.log(prompt("What year did Jobs introduce the 1st computer on the Macintosh?"));
+console.log(prompt("What year did Jobs introduce the iPhone 4?"));
+console.log(prompt("When was MacWorld created?"));
+console.log(prompt("When did Steve Jobs die?"));
+
+vorsitetitle = alert (" We received all your answers, thank you very much!!!" );
+
